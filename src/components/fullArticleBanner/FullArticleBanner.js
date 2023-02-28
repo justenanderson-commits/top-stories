54a51d8f4832cca003663e9b8ac208592032abca
Justen Anderson <justenanderson@gmail.com>
2023-02-28 16:45:07 -0600

    Add section to full article banner.

--- src/components/fullArticleBanner/FullArticleBanner.js
+++ src/components/fullArticleBanner/FullArticleBanner.js
@@ -5,6 +5,7 @@ const FullArticleBanner = () => {
     <div className="full-article-banner">
       <h2>Full Headline</h2>
       <h4>Full abstract</h4>
+      <h5>Date published</h5>
       <h5>Section</h5>
     </div>
   )
