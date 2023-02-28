357b7672d5c965bccc6bac66c869a108d1dc9cb0
Justen Anderson <justenanderson@gmail.com>
2023-02-28 15:53:19 -0600

    Add basic setup and styling for summary card.

--- src/components/summaryCard/SummaryCard.js
+++ src/components/summaryCard/SummaryCard.js
@@ -1,8 +1,13 @@
 import './SummaryCard.css'
 
 const SummaryCard = () => {
-  return <div>SummaryCard</div>
+  return (
+    <div className="summary-card">
+      <h3> Headline </h3>
+      <p> Date and time published</p>
+      <p> Section</p>
+    </div>
+  )
 }
 
 export default SummaryCard
- 
