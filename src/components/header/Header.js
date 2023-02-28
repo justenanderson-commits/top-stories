042d9657f2a0057a8b5b7860097512c7178974f3
Justen Anderson <justenanderson@gmail.com>
2023-02-28 15:36:50 -0600

    Add basic header styling.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,14 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>New York Times Top Stories</h1>
+      </Link>
+    </div>
+  )
 }
 
 export default Header
