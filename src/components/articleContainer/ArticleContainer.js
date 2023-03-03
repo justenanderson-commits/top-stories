edc6581011cf4bce5878823a03d2793038767fa5
Justen Anderson <justenanderson@gmail.com>
2023-03-03 15:18:52 -0700

    Fix article container spacing.

--- src/components/articleContainer/ArticleContainer.js
+++ src/components/articleContainer/ArticleContainer.js
@@ -18,7 +18,7 @@ const ArticleContainer = ({ foundArticle }) => {
           </Link>
         </div>
         <div className="article-image">
-          <img src={foundArticle.multimedia[1].url} width="100px" />
+          <img src={foundArticle.multimedia[1].url} alt={foundArticle.multimedia[1].caption}/>
           <div className="image-caption">
             {' '}
             {foundArticle.multimedia[1].caption}{' '}
