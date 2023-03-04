78bc9b6135a616fa68a3b175bcd427265a30c58c
Justen Anderson <justenanderson@gmail.com>
2023-03-04 13:03:12 -0700

    Remove duplicates from all sections.

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -1,7 +1,7 @@
 import './DropDown.css'
 
 const DropDown = ({section}) => {
-  console.log('Section :', section)
+  // console.log('Section :', section)
   return <option className="drop-down" value={section}> {section} </option>
 }
 
