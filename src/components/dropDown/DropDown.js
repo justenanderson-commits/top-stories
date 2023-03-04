41236e237aabb47b0c29cba5d7101204c8743bb1
Justen Anderson <justenanderson@gmail.com>
2023-03-04 12:48:50 -0700

    Populate the drop down options dynamically.

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -2,7 +2,7 @@ import './DropDown.css'
 
 const DropDown = ({section}) => {
   console.log('Section :', section)
-  return <option className="drop-down" value={section}> Section: {section} </option>
+  return <option className="drop-down" value={section}> {section} </option>
 }
 
 export default DropDown
