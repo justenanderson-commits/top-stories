6806436eaaa17b85ef760980221d341b2c4187f8
Justen Anderson <justenanderson@gmail.com>
2023-03-04 13:17:50 -0700

    Remove comments, consoles, and refactored.

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -1,7 +1,6 @@
 import './DropDown.css'
 
 const DropDown = ({section}) => {
-  // console.log('Section :', section)
   return <option className="drop-down" value={section}> {section} </option>
 }
 
